7af060e143aebb7aaedf5e3304af5c988483afc9
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-05-20 19:39:20 +0530

    New program writen for constant

---
+++ Assignment/Upload 01/06-Constants/Constants.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+#define MY_PI 3.1415926535897932
+
+#define AMC_STRING "AstroMediComp RTR" 
+
+// If First Constant Is not Assigened A value , It is assumed to be 0 ie. 'Sunday' Will be 0 
+// And The Rest Of the constant Are assiged consecutive integer value from 0 onward ie 'Monday' Will be 1 , 'Tuesday' will be 2 and so on 
