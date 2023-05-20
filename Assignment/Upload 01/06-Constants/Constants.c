d2da94f844211d54ec809d8601bfda2d3cc653f5
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-05-20 19:42:01 +0530

    added enum

--- Assignment/Upload 01/06-Constants/Constants.c
+++ Assignment/Upload 01/06-Constants/Constants.c
@@ -6,3 +6,17 @@
 
 // If First Constant Is not Assigened A value , It is assumed to be 0 ie. 'Sunday' Will be 0 
 // And The Rest Of the constant Are assiged consecutive integer value from 0 onward ie 'Monday' Will be 1 , 'Tuesday' will be 2 and so on 
+
+printf("Harshal Lakare \n\n");
+
+//Un-named enums 
+enum
+{
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+};
