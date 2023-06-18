ee83f7f9deec59ae0e133a18e427ff43d3227e60
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-06-18 23:13:35 +0530

    table using for loop and 1D array

---
+++ Assignment/Upload 07/table.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int num;
+	printf("Enter a number to print its table\n");
+	scanf("%d", &num);
+	printf("printing table of %d : \n\n", num);
+
+	int a[10];
+	int i; 
+	for (i = 0; i < 10; i++)
+	{
+		a[i] = num * (i + 1);
+		printf("%d\n", a[i]);
+	}
+	return(0);
+}
