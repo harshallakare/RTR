6dff7eb4d74e4598523e7403ccaafe2fc5fe69d7
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-07-06 23:23:06 +0530

    User Input - Single Struct Variable

--- Assignment/Upload 10/12-StructsAndUnions/03-UserInputData/01-SingleStructVariable/SingleStructVariable-C.c
+++ Assignment/Upload 10/12-StructsAndUnions/03-UserInputData/01-SingleStructVariable/SingleStructVariable-C.c
@@ -1,44 +1,44 @@
 #include<stdio.h>
 #include<conio.h>
 
-struct MyData
+struct hbl_MyData
 {
-	int i; 
-	float f; 
-	double d;
-	char ch; 
+	int hbl_i;
+	float hbl_f;
+	double hbl_d;
+	char hbl_ch;
 
 };
 
 int main(void)
 {
 	//varaible declarations 
-	struct MyData data; // Declarating a single struct variable 
+	struct hbl_MyData hbl_data; // Declarating a single struct variable 
 
 	//code 
 	//User Input For Vlaue of data members of 'struct MyData'
 	printf("Harshal Lakare\n\n");
 	printf("\n\n");
 
-	printf("Enter Interger Value for data member 'i' of 'struct Mydata' : \n");
-	scanf("%d", &data.i);
+	printf("Enter Interger Value for data member 'hbl_i' of 'struct hbl_Mydata' : \n");
+	scanf("%d", &hbl_data.hbl_i);
 
-	printf("Enter Floating-Point Value For data Meber 'f' of 'struct MyData' : \n");
-	scanf("%f", &data.f);
+	printf("Enter Floating-Point Value For data Meber 'hbl_f' of 'struct hbl_MyData' : \n");
+	scanf("%f", &hbl_data.hbl_f);
 
-	printf("Enter 'Double' Value for Data Member 'd' of 'struct MyData' : \n");
-	scanf("%lf", &data.d);
+	printf("Enter 'Double' Value for Data Member 'hbl_d' of 'struct hbl_MyData' : \n");
+	scanf("%lf", &hbl_data.hbl_d);
 
-	printf("Enter character value for data member 'c' of 'struct MyData' : \n");
-	data.ch = getch();
+	printf("Enter character value for data member 'c' of 'struct hbl_MyData' : \n");
+	hbl_data.hbl_ch = getch();
 
 	//Display Value of data Members of 'struct MyData'
 	printf("\n\n");
 	printf("Data Members of 'struct MyData' Are :\n\n");
-	printf("i = %d\n", data.i);
-	printf("f = %f\n", data.f);
-	printf("d = %lf\n", data.d);
-	printf("c = %c\n\n", data.ch);
+	printf("hbl_i = %d\n", hbl_data.hbl_i);
+	printf("hbl_f = %f\n", hbl_data.hbl_f);
+	printf("hbl_d = %lf\n", hbl_data.hbl_d);
+	printf("hbl_c = %c\n\n", hbl_data.hbl_ch);
 
 	return(0);
 }
