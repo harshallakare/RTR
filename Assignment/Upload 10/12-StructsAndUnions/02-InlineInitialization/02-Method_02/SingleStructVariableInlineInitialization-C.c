8b9efde165c1c93eb93d2363d26882e5aecb4f63
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-07-06 23:02:22 +0530

    Single Struct Variable Inline Initialization- Method 2

--- Assignment/Upload 10/12-StructsAndUnions/02-InlineInitialization/02-Method_02/SingleStructVariableInlineInitialization-C.c
+++ Assignment/Upload 10/12-StructsAndUnions/02-InlineInitialization/02-Method_02/SingleStructVariableInlineInitialization-C.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 //Defining Struct 
-struct MyData
+struct hbl_MyData
 {
-	int i; 
-	float f; 
-	double d; 
-	char c;
+	int hbl_i;
+	float hbl_f;
+	double hbl_d;
+	char hbl_c;
 };
 
 
-struct MyData data = {9,8.2f, 9.61998,'P'};
+struct hbl_MyData hbl_data = {9,8.2f, 9.61998,'P'};
 
 int main(void)
 {
@@ -18,10 +18,10 @@ int main(void)
 	//Displaying Values of the Data Memeber of 'Struct MyData'
 	printf("Harshal Lakare\n\n");
 	printf("\n\n");
-	printf("i = %d\n", data.i);
-	printf("f = %f\n", data.f);
-	printf("d = %lf\n", data.d);
-	printf("c = %c\n", data.c);
+	printf("hbl_i = %d\n", hbl_data.hbl_i);
+	printf("hbl_f = %f\n", hbl_data.hbl_f);
+	printf("hbl_d = %lf\n", hbl_data.hbl_d);
+	printf("hbl_c = %c\n", hbl_data.hbl_c);
 
 	return(0);
 }
