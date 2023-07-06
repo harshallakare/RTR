19a188209d9dbb0d973304bca2f63a3d333744b5
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-07-06 22:59:39 +0530

    StructsAndUnions - SingleStruct Variable Inline Initialization

--- Assignment/Upload 10/12-StructsAndUnions/02-InlineInitialization/01-Method_01/SingleStructVariableInlineInitialization.c
+++ Assignment/Upload 10/12-StructsAndUnions/02-InlineInitialization/01-Method_01/SingleStructVariableInlineInitialization.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 //DEfining Struct 
-struct MyData
+struct hbl_MyData
 {
-	int i; 
-	float f; 
-	double d; 
-	char c; 
+	int hbl_i;
+	float hbl_f;
+	double hbl_d;
+	char hbl_c;
 
-} data = {30,4.5f,11.451995,'A'}; //Inline initializations of struct variable 'data' of type 'struct Mydata'
+} hbl_data = {30,4.5f,11.451995,'A'}; //Inline initializations of struct variable 'data' of type 'struct Mydata'
 
 int main(void)
 {
@@ -16,11 +16,11 @@ int main(void)
 	//Displaying Vlaue Of The Data Members of 'struct MyData'
 	printf("Harshal Lakare \n\n");
 	printf("\n\n");
-	printf("Data Members Of 'struct MyData' Are :\n\n");
-	printf("i = %d\n", data.i);
-	printf("f = %f\n", data.f);
-	printf("d = %lf\n", data.d);
-	printf("c = %c\n", data.c);
+	printf("Data Members Of 'struct hbl_MyData' Are :\n\n");
+	printf("hbl_i = %d\n", hbl_data.hbl_i);
+	printf("hbl_f = %f\n", hbl_data.hbl_f);
+	printf("hbl_d = %lf\n", hbl_data.hbl_d);
+	printf("hbl_c = %c\n", hbl_data.hbl_c);
 
 	return(0);
 }
