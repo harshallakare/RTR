c44c1b0098f410099398bbf8385b0ae5545d651e
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-07-06 23:05:08 +0530

    Single Struct Variable Initialization - Method 3

--- Assignment/Upload 10/12-StructsAndUnions/02-InlineInitialization/03-Method_03/SingleStructVariableInitialization.c
+++ Assignment/Upload 10/12-StructsAndUnions/02-InlineInitialization/03-Method_03/SingleStructVariableInitialization.c
@@ -4,23 +4,23 @@
 int main(void)
 {
 	//Defining Struct 
-	struct MyData
+	struct hbl_MyData
 	{
-		int i; 
-		float  f; 
-		double d; 
-		char c; 
+		int hbl_i;
+		float  hbl_f;
+		double hbl_d;
+		char hbl_c;
 
-	} data = {5,9.1f,3.78623, 'N'};
+	} hbl_data = {5,9.1f,3.78623, 'N'};
 
 	//Displaying Values Of the Data Members of 'struct MyData'
 	printf("Harshal Lakare\n\n");
 	printf("\n\n");
 	printf("Data Members of 'struct MyData' Are : \n\n");
-	printf("i = %d\n", data.i);
-	printf("i = %f\n", data.f);
-	printf("i = %lf\n", data.d);
-	printf("i = %c\n\n", data.c);
+	printf("hbl_i = %d\n", hbl_data.hbl_i);
+	printf("hbl_f = %f\n", hbl_data.hbl_f);
+	printf("hbl_d = %lf\n", hbl_data.hbl_d);
+	printf("hbl_c = %c\n\n", hbl_data.hbl_c);
 	
 	return(0);
 
