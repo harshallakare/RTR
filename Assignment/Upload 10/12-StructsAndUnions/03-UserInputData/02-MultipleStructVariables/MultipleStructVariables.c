fde100be4abe175591e42ed2766a7642ac72cc3c
Harshal <Harshal@DESKTOP-3GC2EN7>
2023-07-06 23:26:55 +0530

    UserInput - Multiple Struct Variables

--- Assignment/Upload 10/12-StructsAndUnions/03-UserInputData/02-MultipleStructVariables/MultipleStructVariables.c
+++ Assignment/Upload 10/12-StructsAndUnions/03-UserInputData/02-MultipleStructVariables/MultipleStructVariables.c
@@ -1,60 +1,60 @@
 #include<stdio.h>
 
-struct MyPoint
+struct hbl_MyPoint
 {
-	int x; 
-	int y; 
+	int hbl_x;
+	int hbl_y;
 };
 
 int main(void)
 {
 	//variable declarations 
-	struct MyPoint point_A, point_B, point_C, point_D, point_E; //declaraing a 5 struct varaible 
+	struct hbl_MyPoint hbl_point_A, hbl_point_B, hbl_point_C, hbl_point_D, hbl_point_E; //declaraing a 5 struct varaible 
 
 	//code 
 	//User Input for the data members of 'struct MyPoint' varaible 'point_A'
 	printf("Harshal Lakare \n\n");
 	printf("\n\n");
-	printf("Enter X-COordinaters for point 'A' : ");
-	scanf("%d", &point_A.x);
-	printf("Enter Y-COordinaters for point 'A' : ");
-	scanf("%d", &point_A.y);
+	printf("Enter X-COordinaters for point 'hbl_A' : ");
+	scanf("%d", &hbl_point_A.hbl_x);
+	printf("Enter Y-COordinaters for point 'hbl_A' : ");
+	scanf("%d", &hbl_point_A.hbl_y);
 
 	//User Input for the data members of 'struct MyPoint' varaible 'point_B'
 	printf("\n\n");
-	printf("Enter X-COordinaters for point 'B' : ");
-	scanf("%d", &point_B.x);
-	printf("Enter Y-COordinaters for point 'B' : ");
-	scanf("%d", &point_B.y);
+	printf("Enter X-COordinaters for point 'hbl_B' : ");
+	scanf("%d", &hbl_point_B.hbl_x);
+	printf("Enter Y-COordinaters for point 'hbl_B' : ");
+	scanf("%d", &hbl_point_B.hbl_y);
 
 	//User Input for the data members of 'struct MyPoint' varaible 'point_C'
 	printf("\n\n");
-	printf("Enter X-COordinaters for point 'C' : ");
-	scanf("%d", &point_C.x);
-	printf("Enter Y-COordinaters for point 'C' : ");
-	scanf("%d", &point_C.y);
+	printf("Enter X-COordinaters for point 'hbl_C' : ");
+	scanf("%d", &hbl_point_C.hbl_x);
+	printf("Enter Y-COordinaters for point 'hbl_C' : ");
+	scanf("%d", &hbl_point_C.hbl_y);
 
 	//User Input for the data members of 'struct MyPoint' varaible 'point_D'
 	printf("\n\n");
-	printf("Enter X-COordinaters for point 'D' : ");
-	scanf("%d", &point_D.x);
-	printf("Enter Y-COordinaters for point 'D' : ");
-	scanf("%d", &point_D.y);
+	printf("Enter X-COordinaters for point 'hbl_D' : ");
+	scanf("%d", &hbl_point_D.hbl_x);
+	printf("Enter Y-COordinaters for point 'hbl_D' : ");
+	scanf("%d", &hbl_point_D.hbl_y);
 
 	//User Input for the data members of 'struct MyPoint' varaible 'point_E'
 	printf("\n\n");
-	printf("Enter X-COordinaters for point 'E' : ");
-	scanf("%d", &point_E.x);
-	printf("Enter Y-COordinaters for point 'E' : ");
-	scanf("%d", &point_E.y);
+	printf("Enter X-COordinaters for point 'hbl_E' : ");
+	scanf("%d", &hbl_point_E.hbl_x);
+	printf("Enter Y-COordinaters for point 'hbl_E' : ");
+	scanf("%d", &hbl_point_E.hbl_y);
 
 	//Displaying Values Of the memeber of 'struct MyPoint' (all varaibles )
 	printf("\n\n");
-	printf("Co-ordinates (x,y) Of Point 'A' are : (%d, %d) \n\n", point_A.x, point_A.y);
-	printf("Co-ordinates (x,y) Of Point 'B' are : (%d, %d) \n\n", point_B.x, point_B.y);
-	printf("Co-ordinates (x,y) Of Point 'C' are : (%d, %d) \n\n", point_C.x, point_C.y);
-	printf("Co-ordinates (x,y) Of Point 'D' are : (%d, %d) \n\n", point_D.x, point_D.y);
-	printf("Co-ordinates (x,y) Of Point 'E' are : (%d, %d) \n\n", point_E.x, point_E.y);
+	printf("Co-ordinates (x,y) Of Point 'A' are : (%d, %d) \n\n", hbl_point_A.hbl_x, hbl_point_A.hbl_y);
+	printf("Co-ordinates (x,y) Of Point 'B' are : (%d, %d) \n\n", hbl_point_B.hbl_x, hbl_point_B.hbl_y);
+	printf("Co-ordinates (x,y) Of Point 'C' are : (%d, %d) \n\n", hbl_point_C.hbl_x, hbl_point_C.hbl_y);
+	printf("Co-ordinates (x,y) Of Point 'D' are : (%d, %d) \n\n", hbl_point_D.hbl_x, hbl_point_D.hbl_y);
+	printf("Co-ordinates (x,y) Of Point 'E' are : (%d, %d) \n\n", hbl_point_E.hbl_x, hbl_point_E.hbl_y);
 
 	return(0);
 
